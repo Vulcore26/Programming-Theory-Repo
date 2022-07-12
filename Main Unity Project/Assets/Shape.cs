eb0024c9d4846925b136851b9b1609378a49ed78
Steven <steven.woods@theeventbusiness.co.uk>
2022-07-12 15:54:26 +0100

    Base shape and UI implemented

---
+++ Main Unity Project/Assets/Shape.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Shape : MonoBehaviour
+{
+    protected MeshFilter filter;
+    protected new MeshCollider collider;
+    protected new MeshRenderer renderer;
+    [SerializeField] internal Mesh mesh;
+    [SerializeField] internal Material[] materials;
+    [SerializeField] float roationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filter = GetComponent<MeshFilter>();
+        collider = GetComponent<MeshCollider>();
+        renderer = GetComponent<MeshRenderer>();
+
+        SetShape();
+    }
+
+    internal void ReturnInput(Text text)
+    {
+        text.text = gameObject.name;
+    }
+
+    // Update is called once per frame
+    internal virtual void SetShape()
+    {
+        filter.mesh = mesh;
+        renderer.materials = materials;
+        collider.sharedMesh = mesh;
+        collider.sharedMesh.RecalculateBounds();
+        collider.sharedMesh.RecalculateNormals();
+    }
+
+    private void Update()
+    {
+        transform.rotation *= Quaternion.Euler(1f * roationSpeed,0, 1f * roationSpeed) ;
+    }
+}
---
+++ Main Unity Project/Assets/UserInput.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public Text text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetAxis("Fire1") != 0)
+        {
+            UserClicked();
+        }
+    }
+
+    private void UserClicked()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if( Physics.Raycast(ray, out hit) )
+        {
+            if( hit.transform.CompareTag("Shape"))
+            {
+                Shape s = hit.transform.GetComponent<Shape>();
+                s.ReturnInput(text);
+            }
+        }
+    }
+}
